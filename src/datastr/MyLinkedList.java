4b58cce5ebe3a6b4e9ffbca15b9a7ca1f482a32d
krisnawa00 <mustafacetin1619@gmail.com>
2025-03-01 15:00:02 +0200

    jaunumi!

---
+++ src/datastr/MyLinkedList.java
@@ -0,0 +1,27 @@
+package datastr;
+
+public class MyLinkedList <Ttype>{
+	private MyNode firstNode = null;// netaisu ne get, net set nav nepieciesams
+	private MyNode lastNode = null;// netaisu ne get, net set nav nepieciesams
+	private int counter = 0;
+	
+	//neveidosu bezargumeta konstruktoru, jo tas ir jau no object klases pieejams
+	
+	public boolean isFull() {
+		try {
+		MyNode<Ttype> newNode = new MyNode (4);
+		}
+		catch (OutOfMemoryError e) {
+			return true;
+		}
+		
+	}
+	
+	public boolean isEmpty() {
+		return (counter==0);
+	}
+	public int howMnayElements() {
+		return counter;
+	}
+	
+}
--- src/datastr/MyNode.java
+++ src/datastr/MyNode.java
@@ -1,5 +1,36 @@
 package datastr;
 
-public class MyNode {
 
+public class MyNode <Ttype> {
+	private Ttype element;
+	private MyNode<Ttype> next = null;
+	
+	public Ttype getElement() {
+		return element;
+	}
+	public MyNode<Ttype> getNext() {
+		return next;
+	}
+	
+	public void setElement(Ttype inputElement) {
+		if(inputElement != null)
+		{
+			element = inputElement;
+		}
+		else
+		{
+			element = (Ttype) new Object();
+		}
+	}
+	public void setNext(MyNode<Ttype> inputNext) {
+		next = inputNext;
+	}
+	
+	public MyNode(Ttype element) {
+		setElement(element);
+	}
+
+	public String toString() {
+		return element + "";
+	}
 }
